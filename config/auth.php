26c3695664d2ec8fd4c7d6ede0bed6b98815d40a
Ivan-Chou <ivanchou715@gmail.com>
2025-03-08 23:14:14 +0800

    Add authentication mechanism(sanctum) to APIs that should be called only after users have logged in

--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,12 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+
+        // Note: effect Unknown
+        'api' => [
+            'driver' => 'sanctum',
+            'provider' => 'users',
+        ]
     ],
 
     /*
